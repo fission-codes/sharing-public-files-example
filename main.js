9314aa3fd6bc48cdaf3920d773af758516dfdbed
Brian Ginsburg <gins@brianginsburg.com>
2021-06-03 16:09:08 -0700

    Upgrade to webnative 0.25.2

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ const publicLinkAnchor = document.getElementById('publicLink');
 const currentLinkAnchor = document.getElementById('currentVersionLink');
 const versions = document.getElementById('versions');
 
-
 let fs;
 let publicLink = null;
 
@@ -18,7 +17,7 @@ const fissionInit = {
       creator: 'bgins'
     },
     fs: {
-      publicPaths: ['examples']
+      public: [webnative.path.directory('examples')]
     }
   }
 };
@@ -33,7 +32,7 @@ webnative.initialize(fissionInit).then(async state => {
 
       fs = state.fs;
 
-      if (await fs.exists('public/examples/shared.txt')) {
+      if (await fs.exists(webnative.path.file("public", "examples", "shared.txt"))) {
 
         /**
          * Set the public link if it has not been set.
@@ -54,7 +53,7 @@ webnative.initialize(fissionInit).then(async state => {
          * Traverse the file history and generate links for all older versions of the file.
          * This will not include the current version of the file.
         */
-        const file = await fs.get('public/examples/shared.txt');
+        const file = await fs.get(webnative.path.file("public", "examples", "shared.txt"));
         const history = await file.history.list();
         history.forEach(async versionMetadata => {
           const version = await file.history.back(versionMetadata.delta);
@@ -67,8 +66,8 @@ webnative.initialize(fissionInit).then(async state => {
         event.preventDefault();
         const content = textInput.value;
 
-        if (fs.exists('public/examples')) {
-          await fs.write('public/examples/shared.txt', content);
+        if (fs.exists(webnative.path.directory("public", "examples"))) {
+          await fs.write(webnative.path.file("public", "examples", "shared.txt"), content);
           await fs.publish();
 
           /**
@@ -89,7 +88,7 @@ webnative.initialize(fissionInit).then(async state => {
           /**
            * Add the last version of the app to the older versions.
            */
-          const file = await fs.get('public/examples/shared.txt');
+          const file = await fs.get(webnative.path.file("public", "examples", "shared.txt"));
           const history = await file.history.list();
           if (history.length > 0) {
             const backOneVersion = await file.history.back(-1);
@@ -110,7 +109,6 @@ webnative.initialize(fissionInit).then(async state => {
   }
 
   signInButton.addEventListener('click', () => {
-    console.log('signing in')
     webnative.redirectToLobby(state.permissions);
   });
 
